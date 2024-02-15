f8b3ed86944f3b393962dc7ddc2a7d58edf0a105
Chumani-Lalasa <chumanilalasa@gmail.com>
2024-02-15 10:20:32 +0530

    thunk

---
+++ src/Redux/Reducer.jsx
@@ -0,0 +1,15 @@
+import { FETCH_DATA } from "./ActionType"
+
+const initState = {
+    data:[],
+}
+const Reducer = (state = initState,{type,payload}) => {
+    switch(type){
+        case FETCH_DATA:
+            return {...state,data:payload}
+        default:
+            return state
+    }
+}
+export default Reducer
+
